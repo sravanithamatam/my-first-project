a4a2b705a949968264238b121ace763926e67927
Sravani Reddy <sravani.thamatam@aveva.com>
2017-02-08 18:01:09 +0530

    Add project files.

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Program> l1 = new List<Program>();
+            l1.Add(new Program());
+            l1.Add(new Program());
+            List<Program> l2 = l1;
+            l2.Add(new Program());
+            Console.WriteLine("Generics Program...........");
+               
+        }
+    }
+}
